ca6099f536a3df25c4d7f0c9e8a16f10727d9094
tushar-upadhya <tusharupadhyay691@gmail.com>
2024-02-19 13:04:07 +0530

    clerk setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <p>home</p>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 };
 
 export default Home;
