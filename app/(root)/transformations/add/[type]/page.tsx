34a069eba592cc87d2c22ba781af0ce5207cd00b
tushar-upadhya <tusharupadhyay691@gmail.com>
2024-02-20 18:53:14 +0530

    connect to database

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,5 +1,12 @@
-const AddTransformationTypePage = () => {
-  return <div>AddTransformationTypePage</div>;
+import Header from "@/components/share/Header";
+import { transformationTypes } from "@/constants";
+
+const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
+  const transformation = transformationTypes[type];
+
+  return (
+    <Header title={transformation.title} subTitle={transformation.subTitle} />
+  );
 };
 
 export default AddTransformationTypePage;
