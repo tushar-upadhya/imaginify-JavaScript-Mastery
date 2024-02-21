bc44a23b03799b01c2fa2a944d2938f29aece11e
tushar-upadhya <tusharupadhyay691@gmail.com>
2024-02-21 21:51:58 +0530

    zod

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,11 +1,33 @@
 import Header from "@/components/share/Header";
+import TransformationForm from "@/components/share/TransformationForm";
 import { transformationTypes } from "@/constants";
+import { getUserById } from "@/lib/actions/user.action";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
+const AddTransformationTypePage = async ({
+  params: { type },
+}: SearchParamProps) => {
+  const { userId } = auth();
+
   const transformation = transformationTypes[type];
 
+  if (!userId) redirect("/sign-in");
+
+  const user = await getUserById(userId);
   return (
-    <Header title={transformation.title} subTitle={transformation.subTitle} />
+    <>
+      <Header title={transformation.title} subTitle={transformation.subTitle} />
+
+      <section className="mt-10">
+        <TransformationForm
+          action="Add"
+          userId={user._id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
+    </>
   );
 };
 
